4126112d46b926696a614b2d5e9645eaeed4864e
15205231 <w92pk@vip.qq.com>
2019-02-28 19:14:08 +0800

    commit login app

--- login/views.py
+++ login/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        temp = {'user':username, 'pwd':password}
+        user_list.append(temp)
+    return render(request, 'index.html', {'data':user_list})
